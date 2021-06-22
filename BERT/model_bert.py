c0a9a09645a316135d3f85e21bfa55ca22efdbb1
rex <hanhung>
2021-06-22 23:20:34 +0800

    remove unused import

--- BERT/model_bert.py
+++ BERT/model_bert.py
@@ -5,7 +5,6 @@ import torch, torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
-from loss import *
 from util import *
 from config_bert import *
 from transformers import *
